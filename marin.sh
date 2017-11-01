e380aa36ef3e1066fc942633b16c5ad5a708bf3a
Brett <brettn1@umbc.edu>
2017-11-01 04:25:47 -0400

    First commit

---
+++ marin.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+############################################################
+#	author: brettn1@umbc.edu							   #	
+#	description: a program that makes noises to sleep to   #
+############################################################
+
+command -v sox > /dev/null 2>&1 || { echo >&2 "I require SoX but it's not installed.  Aborting."; exit 1; }
+
+simple_prefrences(){
+	OPTIONS="Brown Pink White Back Quit"
+	select opt in $OPTIONS; do
+	    if [ "$opt" = "Brown" ]; then
+ 			echo [Press Ctrl-C to stop noise]
+ 			command play -n synth brownnoise #> /dev/null 2>&1
+	     elif [ "$opt" = "Pink" ]; then
+	     	echo [Press Ctrl-C to stop noise]
+			command play -n synth pinknoise #> /dev/null 2>&1
+	     elif [ "$opt" = "White" ]; then
+	     	echo [Press Ctrl-C to stop noise]
+	  		play -n synth whitenoise #> /dev/null 2>&1
+	  	 elif [ "$opt" = "Back" ]; then
+	  	 	main
+	  	 elif [ "$opt" = "Quit" ]; then
+	  		echo Hate to see you go but love to watch ya leave \;\)
+	  		exit	     
+	     else
+	       #clear
+	       echo bad option
+	     fi
+	done
+}
+
+main(){
+echo MARINS NOISEE SCRIPT
+OPTIONS="MakeSimpleNoise MakeComplecksNoise Quit"
+	select opt in $OPTIONS; do
+	    if [ "$opt" = "Quit" ]; then
+	      echo Hate to see you go but love to watch ya leave \;\)
+	      exit
+	     elif [ "$opt" = "MakeSimpleNoise" ]; then
+	      simple_prefrences
+	     elif [ "$opt" = "MakeComplecksNoise" ]; then
+	      echo UNDER CONSTRUCTION
+	      #complecks_prefrences
+	     else
+	       #clear
+	       echo bad option
+	     fi
+	done
+}
+
+main
